acb1a6d40c1d6c8e093d8e388b0675dcfdd8f551
Ayush-refactorQ <ayush.verma@refactorq.com>
2023-04-22 19:23:01 +0530

    comit

--- src/components/AllBlog.jsx
+++ src/components/AllBlog.jsx
@@ -1,32 +1,55 @@
-import React from 'react';
-import { blogs } from '../data';
-import BlogCard from './BlogCard';
-import { Helmet } from 'react-helmet-async';
+import React from "react";
+import { blogs } from "../data";
+import BlogCard from "./BlogCard";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const AllBlogs = () => {
   return (
     <>
-      <Helmet>
-        <title>All Blogs</title>
-        <meta
-          name="description"
-          content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="www.designcode.io" />
-        <meta
-          property="og:title"
-          content="Design+Code - Learn to design and code React and Swift apps"
-        />
-        <meta
-          property="og:description"
-          content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
-        />
-        <meta
-          property="og:image"
-          content="https://designcode.io/cloud/v2/twitter.jpg"
-        />
-      </Helmet>
+      <HelmetProvider>
+        <Helmet>
+          <title>
+            Design+Code - Learn to design and code React and Swift apps
+          </title>
+          <meta
+            name="description"
+            content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+          />
+
+          {/* Twitter meta tags below */}
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:image"
+            content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDDhvOcl3hUUOO_TNsJjfi5-Bbe3Hh3p211w&usqp=CAU"
+          />
+          <meta
+            name="twitter:title"
+            content="Design+Code - Learn to design and code React and Swift apps"
+          />
+          <meta name="twitter:creator" content="@diepsteph" />
+          <meta name="twitter:site" content="@diepsteph" />
+          <meta
+            name="twitter:description"
+            content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+          />
+
+          {/* Facebook meta tags below */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="www.designcode.io" />
+          <meta
+            property="og:title"
+            content="Design+Code - Learn to design and code React and Swift apps"
+          />
+          <meta
+            property="og:description"
+            content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+          />
+          <meta
+            property="og:image"
+            content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDDhvOcl3hUUOO_TNsJjfi5-Bbe3Hh3p211w&usqp=CAU"
+          />
+        </Helmet>
+      </HelmetProvider>
       <div className="flex justify-center items-center flex-col w-full px-10">
         <h2 className="text-4xl font-bold my-10">All Blogs</h2>
         <div className="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import { blogs } from '../data';
-import { Helmet } from 'react-helmet-async';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { blogs } from "../data";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Blog = () => {
   const { id: blogId } = useParams();
@@ -18,15 +18,50 @@ const Blog = () => {
   return (
     currentBlog && (
       <>
+      <HelmetProvider>
         <Helmet>
-          <title>{currentBlog?.title}</title>
-          <meta name="description" content={currentBlog?.description} />
+          <title>
+            Design+Code - Learn to design and code React and Swift apps
+          </title>
+          <meta
+            name="description"
+            content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+          />
+
+          {/* Twitter meta tags below */}
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:image"
+            content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDDhvOcl3hUUOO_TNsJjfi5-Bbe3Hh3p211w&usqp=CAU"
+          />
+          <meta
+            name="twitter:title"
+            content="Design+Code - Learn to design and code React and Swift apps"
+          />
+          <meta name="twitter:creator" content="@diepsteph" />
+          <meta name="twitter:site" content="@diepsteph" />
+          <meta
+            name="twitter:description"
+            content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+          />
+
+          {/* Facebook meta tags below */}
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="www.blogs.io" />
-          <meta property="og:title" content={currentBlog?.title} />
-          <meta property="og:description" content={currentBlog?.description} />
-          <meta property="og:image" content={currentBlog?.image} />
+          <meta property="og:url" content="www.designcode.io" />
+          <meta
+            property="og:title"
+            content="Design+Code - Learn to design and code React and Swift apps"
+          />
+          <meta
+            property="og:description"
+            content="Learn design and code by building real apps with React and Swift. Complete courses about UI design, web and iOS development using Figma, CSS, React Hooks and SwiftUI."
+          />
+          <meta
+            property="og:image"
+            content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDDhvOcl3hUUOO_TNsJjfi5-Bbe3Hh3p211w&usqp=CAU"
+          />
         </Helmet>
+        </HelmetProvider>
         <div className="flex flex-col max-w-4xl mx-auto">
           <h2 className="text-3xl font-bold text-center my-11">
             {currentBlog?.title}
