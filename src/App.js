8b2d3a4683ba90350b1d2801e0d5b543c148e7de
Ayush-refactorQ <ayush.verma@refactorq.com>
2023-04-22 19:48:22 +0530

    hash

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import AllBlogs from './components/AllBlog';
-import Blog from './components/Blog';
-import { HelmetProvider } from 'react-helmet-async';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import AllBlogs from "./components/AllBlog";
+import Blog from "./components/Blog";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
     <div className="App">
       <HelmetProvider>
-        <Routes>
-          <Route path="/" element={<AllBlogs />} />
-          <Route path="/:id" element={<Blog />} />
-        </Routes>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+            <Route path="/" element={<AllBlogs />} />
+            <Route path="/:id" element={<Blog />} />
+          </Routes>
+        </HashRouter>
       </HelmetProvider>
     </div>
   );
